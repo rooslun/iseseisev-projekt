6454de3ea63f309b370b52709fbb82a56c2e9c57
Roos-Marie Lunden <roosml@tlu.ee>
2020-05-19 21:45:49 +0300

    update
    update

--- skript.js
+++ skript.js
@@ -12,28 +12,32 @@ shopButton.addEventListener("click", addShop);
 
 //FUnktsions
 
-function addTodo(event) {
+function addShop(event) {
+    //console.log("hello");
     // Prevent form from submitting 
     event.preventDefault();
     // shop DIV
+
     const shopDiv = document.createElement("div");
     shopDiv.classList.add("shop");
+
     // create LI
-    const newShop = document.createElement("li");
-    newShop.innerText = "hey";
-    newShop.classList.add("shop-item");
+    const newShop = document.createElement('li');
+    newShop.innerText = 'hey';
+    newShop.classList.add('shop-item');
     shopDiv.appendChild(newShop);
+
     // ccheck mark button
-    const completedButton = document.createElement("Button");
-    completedButton.innterText = '<i class="fas fa-check"></i>'
-    //_____
+    const completedButton = document.createElement('Button');
+    completedButton.innerText = 'TÕMBA MAHA'
     completedButton.classList.add("complete-btn");
     shopDiv.appendChild(completedButton);
     // ccheck trash mark button
-    const trasButton = document.createElement("Button");
-    trashButton.innterText = '<i class="fas fa-trash"></i>';
+    const trashButton = document.createElement('Button');
+    trashButton.innerText = 'KUSTUTA';
+//<i class="fas fa-trash-alt"></i>
     trashButton.classList.add("complete-btn");
     shopDiv.appendChild(trashButton);
     //APPEND to list
-    shopList.appendChild(trashButton);
+    shopList.appendChild(shopDiv);
 }
