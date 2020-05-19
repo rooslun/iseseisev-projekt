a94dfa5cb76f7ac4b73309003d7e867ac0bad4fc
Roos-Marie Lunden <roosml@tlu.ee>
2020-05-19 20:39:06 +0300

    update
    update

--- skript.js
+++ skript.js
@@ -1,3 +1,29 @@
-const itemToAdd = document.getElementById("itemToAdd");
-const addButton = document.getElementById("addButton");
-const itemList = document.getElementById("list");
+// Selektorid
+const shopInput = document.querySelector(".shop-input");
+const shopButton = document.querySelector(".shop-button");
+const shopList = document.querySelector(".shop-list");
+
+
+
+//EVEnt listeners
+
+shopButton.addEventListener("click", addShop);
+
+
+//FUnktsions
+
+function addTodo(event) {
+    // Prevent form from submitting 
+    event.preventDefault();
+    // shop DIV
+    const shopDiv = document.createElement("div");
+    shopDiv.classList.add("shop");
+    // create LI
+    const newShop = document.createElement("li");
+    newShop.innerText = "hey";
+    newShop.classList.add("shop-item");
+    shopDiv.appendChild(newShop);
+    // ccheck mark button
+    const completedButton = document.createElement("Button");
+    completedButton.innterText = '<i class="fas fa-check"></i>'
+}
