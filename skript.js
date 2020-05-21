014c0866bfea5f85a324120ab5ffe25bd20febe5
Jan Morten Kivi <55129090+janmortenk@users.noreply.github.com>
2020-05-21 16:28:11 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -7,7 +7,7 @@ const filterOption = document.querySelector(".filter-shop");
 
 
 //EVEnt listeners
-
+document.addEventListener("DOMContentLoad", getShops);
 shopButton.addEventListener("click", addShop);
 shopList.addEventListener('click', deleteCheck);
 filterOption.addEventListener("click", filterShop);
@@ -35,6 +35,9 @@ function addShop(event) {
     completedButton.innerText = 'TÕMBA MAHA'
     completedButton.classList.add("complete-btn");
     shopDiv.appendChild(completedButton);
+    //local storagesse lisamine
+    saveLocalShops(shopInput.value);
+
     // ccheck trash mark button
     const trashButton = document.createElement('Button');
     trashButton.innerText = 'X';
@@ -56,6 +59,7 @@ function deleteCheck(e){
     if(item.classList[0] === 'trash-btn'){
         const shop = item.parentElement;
         shop.classList.add("fall");
+        removeLocalShops(shop);
         //animation
         shop.addEventListener('transitionend', function(){
             shop.remove();
@@ -66,24 +70,96 @@ function deleteCheck(e){
     // Tõmba maha  ?? ei tööta
     if(item.classList[0] === "complete-btn"){
         const shop = item.parentElement;
-        shop.classList.toggle('completed');
+        shop.classList.toggle("completed");
     }
 }
 
+//filtrid
 function filterShop(e){
     const shops = shopList.childNodes;
-    console.log(shops);
     shops.forEach(function(shop){
-        switch(e.target.value){
+        switch (e.target.value) {
             case "all":
-                shop.style.display = "flex"
+                shop.style.display = "flex";
                 break;
             case "completed":
                 if(shop.classList.contains("completed")){
                     shop.style.display = "flex";
-                }else{
+                } else {
                     shop.style.display = "none";
                 }
+                break;
+            case "uncompleted":
+                if(!shop.classList.contains('completed')){
+                    shop.style.display = "flex";
+                } else {
+                    shop.style.display = "none";
+                }
+                break;
         }
     });
 }
+
+function saveLocalShops(shop){
+    //kontroll, kas on juba olemas
+    let shops;
+    if(localStorage.getItem('shops') == null){
+        shops = [];
+    }else{
+        shops = JSON.parse(localStorage.getItem('shops'));
+    }
+
+    shops.push(shop);
+    localStorage.setItem('todos', JSON.stringify(shops));
+}
+function getShops(){
+    let shops;
+    if(localStorage.getItem('shops') == null){
+        shops = [];
+    }else{
+        shops = JSON.parse(localStorage.getItem('shops'));
+    }
+    shops.forEach(function(shop){ 
+            // shop DIV
+
+        const shopDiv = document.createElement("div");
+        shopDiv.classList.add("shop");
+
+        // create LI
+        const newShop = document.createElement('li');
+        newShop.innerText = shop;
+        newShop.classList.add('shop-item');
+        shopDiv.appendChild(newShop);
+
+        // ccheck mark button
+        const completedButton = document.createElement('Button');
+        completedButton.innerText = 'TÕMBA MAHA'
+        completedButton.classList.add("complete-btn");
+        shopDiv.appendChild(completedButton);
+        //local storagesse lisamine
+
+
+        // ccheck trash mark button
+        const trashButton = document.createElement('Button');
+        trashButton.innerText = 'X';
+        //<i class="fas fa-trash-alt"></i>
+        trashButton.classList.add("trash-btn");
+        shopDiv.appendChild(trashButton);
+        //APPEND to list
+        shopList.appendChild(shopDiv);
+
+
+    });
+}
+function removeLocalShops(shop){
+    let shops;
+    if(localStorage.getItem('shops') == null){
+        shops = [];
+    }else{
+        shops = JSON.parse(localStorage.getItem('shops'));
+    }
+    const shopIndex = shop.children[0].innerText;
+    shops.splice(shops.indexOf(shopIndex), 1);
+    localStorage.setItem("shops", JSON.stringify(shops));
+}
+
