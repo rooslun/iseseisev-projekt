6d4cffff9b9a700dda22638402deb80d4c7bb00f
Roos-Marie Lunden <roosml@tlu.ee>
2020-05-22 13:24:25 +0300

    update
    update

--- skript.js
+++ skript.js
@@ -326,3 +326,29 @@ class Calculator {
       var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
       qoutput.innerHTML = randomQuote;
   })
+
+
+  // Muusika
+  function Song1() {
+    document.getElementById('mysong').src = "Kuule.mp3";
+  }
+  
+  function Song2() {
+    document.getElementById('mysong').src = "Beautiful.mp3";
+  }
+
+  // Tausta pic
+
+const IMAGE_URLS = {
+  nature: 'https://images.unsplash.com/photo-1501854140801-50d01698950b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2600&q=80',
+  pencils: 'https://images.unsplash.com/photo-1489844097929-c8d5b91c456e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80',
+};
+// Sets the background image
+const setBackground = (image) => {
+  document.body.style.background = "url('"+IMAGE_URLS.[image]+"')";
+};
+if (isWeekend) {
+  setBackground('nature');
+} else {
+  setBackground('pencils');
+}
